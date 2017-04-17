339f4e027c76b25280e1bd875ce03838e7ac6526
Sandeep Kumar <sandeep201451066@users.noreply.github.com>
2017-04-17 21:26:34 +0530

    Update example.c

--- example.c
+++ example.c
@@ -1,6 +1,12 @@
+include $stdio.h$
+function as():
+int a=6;
+return 2;
+end
+void main():
 int k,lm,lm;
 int k=4;
-a=5;
+a=5-6*k+5/8*4;
 if(a==b):
 	a=5;
 if(a==b):
@@ -14,7 +20,7 @@ else
 end
 end
 end
-
+a=as();
 
 for(a=1;a<b;a=a+1):
 	a=4;
@@ -39,6 +45,28 @@ end
 end
 	int a=5;
 end
-
-
-
+switch(3):
+case 3:
+a=5;
+break;
+case 5:
+while(a>b):
+	for(a=1;a<b;a=a+1):
+	a=4;
+if(a==b):
+	a=5;
+else
+	if(a==b):
+	a=5;
+end
+end
+end
+	int a=5;
+end
+a=5;
+break;
+case 4:
+a=5;
+break;
+end
+end
